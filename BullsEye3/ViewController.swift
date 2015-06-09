26a9414b8e050813294d908684c7db7177f1688d
Ed Katzman <tangobango@comcast.net>
2015-06-09 14:43:58 -0500

    Initial commit

--- BullsEye3/ViewController.swift
+++ BullsEye3/ViewController.swift
@@ -10,14 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var currentValue: Int = 50
+    var targetValue: Int = 0
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        startNewRound()
+        updateLabels()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func showAlert() {
+        let message = "The value of the slider is: \(currentValue)" +
+                        "\nThe target value is: \(targetValue)"
+        
+        let alert = UIAlertController(title: "Hello, World", message: message, preferredStyle: .Alert)
+        let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        alert.addAction(action)
+        presentViewController(alert, animated: true, completion: nil)
+        
+        startNewRound()
+        updateLabels()
+    }
+    
+    @IBAction func sliderMoved(slider: UISlider) {
+        currentValue = lroundf(slider.value)
+    }
+    
+    func startNewRound() {
+        
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
+        
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
     }
 
 
