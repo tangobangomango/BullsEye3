77cd9be7f6fe34d9671c17b3989350a1793d18ff
Ed Katzman <tangobango@comcast.net>
2015-06-10 17:24:43 -0500

    Sized

--- BullsEye3/AboutViewController.swift
+++ BullsEye3/AboutViewController.swift
@@ -9,11 +9,28 @@
 import UIKit
 
 class AboutViewController: UIViewController {
+    
+    @IBOutlet weak var webView: UIWebView!
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let htmlfile = NSBundle.mainBundle().pathForResource("BullsEye", ofType: "html") {
+            let htmlData = NSData(contentsOfFile: htmlfile)
+            let baseURL = NSURL.fileURLWithPath(NSBundle.mainBundle().bundlePath)
+            
+            webView.loadData(htmlData, MIMEType: "text/html", textEncodingName: "UTF-8", baseURL: baseURL)
+        }
+    }
+    
+    
 
     @IBAction func close() {
         dismissViewControllerAnimated(true, completion: nil)
     }
-
+    
+   
     /*
     // MARK: - Navigation
 
--- BullsEye3/ViewController.swift
+++ BullsEye3/ViewController.swift
@@ -25,6 +25,22 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         startOver()
         
+        let thumbImageNormal = UIImage(named: "SliderThumb-Normal")
+        slider.setThumbImage(thumbImageNormal, forState: .Normal)
+        let thumbImageHighlighted = UIImage(named: "SliderThumb-Highlighted")
+        slider.setThumbImage(thumbImageHighlighted, forState: .Highlighted)
+        let insets = UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
+        if let trackLeftImage = UIImage(named: "SliderTrackLeft") {
+            let trackLeftResizable =
+            trackLeftImage.resizableImageWithCapInsets(insets)
+            slider.setMinimumTrackImage(trackLeftResizable, forState: .Normal)
+        }
+        if let trackRightImage = UIImage(named: "SliderTrackRight") {
+            let trackRightResizable =
+            trackRightImage.resizableImageWithCapInsets(insets)
+            slider.setMaximumTrackImage(trackRightResizable, forState: .Normal)
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
