b04026d923d9ab010413fa2f9051e9dfc54c7e77
Ed Katzman <tangobango@comcast.net>
2015-06-10 12:07:35 -0500

    background in place

---
+++ BullsEye3/AboutViewController.swift
@@ -0,0 +1,27 @@
+//
+//  AboutViewController.swift
+//  BullsEye3
+//
+//  Created by Anne West on 6/10/15.
+//  Copyright (c) 2015 TDG. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    @IBAction func close() {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- BullsEye3/ViewController.swift
+++ BullsEye3/ViewController.swift
@@ -10,17 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var currentValue: Int = 50
-    var targetValue: Int = 0
+    var currentValue = 50
+    var targetValue = 0
+    var score = 0
+    var round = 0
     
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        startNewRound()
-        updateLabels()
+        startOver()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,16 +33,40 @@ class ViewController: UIViewController {
     }
     
     @IBAction func showAlert() {
-        let message = "The value of the slider is: \(currentValue)" +
-                        "\nThe target value is: \(targetValue)"
         
-        let alert = UIAlertController(title: "Hello, World", message: message, preferredStyle: .Alert)
-        let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        let difference = abs(currentValue - targetValue)
+        var points = 100 - difference
+        
+        
+        
+        var title: String
+        if difference == 0 {
+            title = "Perfect!"
+            points += 100
+        } else if difference < 5 {
+            title = "Very close!"
+            if difference == 1 {
+                points += 50
+            }
+        } else if difference < 10 {
+            title = "Not bad."
+        } else {
+            title = "Hmmm"
+        }
+        
+        score += points
+        
+        let message = "You scored \(points) points"
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        let action = UIAlertAction(title: "OK", style: .Default, handler: { action in
+                                                                            self.startNewRound()
+                                                                            self.updateLabels()
+                                                                            })
         alert.addAction(action)
         presentViewController(alert, animated: true, completion: nil)
         
-        startNewRound()
-        updateLabels()
+        
     }
     
     @IBAction func sliderMoved(slider: UISlider) {
@@ -46,7 +74,7 @@ class ViewController: UIViewController {
     }
     
     func startNewRound() {
-        
+        round += 1
         targetValue = 1 + Int(arc4random_uniform(100))
         currentValue = 50
         slider.value = Float(currentValue)
@@ -55,6 +83,19 @@ class ViewController: UIViewController {
     
     func updateLabels() {
         targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
+    }
+    
+    func startNewGame() {
+      score = 0
+      round = 0
+      startNewRound()
+    }
+    
+    @IBAction func startOver() {
+        startNewGame()
+        updateLabels()
     }
 
 
